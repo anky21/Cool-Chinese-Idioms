04ed1f8cebbc05676310a6b79dd14211a17c4543
Anky <anky_au@hotmail.com>
2017-02-18 22:21:29 +1000

    feat: Build UI for Idiom of the Day feature

--- app/src/main/java/me/anky/coolchineseidioms/HomeFragment.java
+++ app/src/main/java/me/anky/coolchineseidioms/HomeFragment.java
@@ -1,17 +1,24 @@
 package me.anky.coolchineseidioms;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    @BindView(R.id.card_view)
+    CardView cardView;
 
 
     public HomeFragment() {
@@ -23,7 +30,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, rootView);
+
+        cardView.setCardBackgroundColor(Color.CYAN);
+
+        return rootView;
     }
 
 }
