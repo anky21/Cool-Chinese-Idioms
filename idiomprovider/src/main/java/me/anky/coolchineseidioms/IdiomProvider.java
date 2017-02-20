cd8cb1571bf674bd282e6478fda31b257cc0cb58
Anky <anky_au@hotmail.com>
2017-02-20 13:16:11 +1000

    feat: Create a Java library to store idioms

---
+++ idiomprovider/src/main/java/me/anky/coolchineseidioms/IdiomProvider.java
@@ -0,0 +1,23 @@
+package me.anky.coolchineseidioms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class IdiomProvider {
+    public static String getIdiom() {
+
+        // Collection of the idioms
+        List<String> jokes = new ArrayList<>();
+        jokes.add("Yi Ming Jing Ren");
+        jokes.add("Hua Long Dian Jing");
+        jokes.add("Yi Xin Yi Yi");
+        jokes.add("Xi Qi Yang Yang");
+        jokes.add("Hao Shi Cheng Shuang");
+
+        // Randomly create an integer according to the size of the String array
+        int rnd = new Random().nextInt(jokes.size());
+
+        return jokes.get(rnd);
+    }
+}
