c7661630e5aa59efd8a12500d62f8fb28de6e970
Anky <anky_au@hotmail.com>
2017-02-21 14:42:30 +1000

    feat: Deploy backend to App Engine

--- app/src/main/java/me/anky/coolchineseidioms/EndpointsAsyncTask.java
+++ app/src/main/java/me/anky/coolchineseidioms/EndpointsAsyncTask.java
@@ -1,6 +1,5 @@
 package me.anky.coolchineseidioms;
 
-import android.content.Context;
 import android.os.AsyncTask;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -19,7 +18,6 @@ import me.anky.coolchineseidioms.backend.myApi.MyApi;
 
 class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
     private static MyApi myApiService = null;
-    private Context context;
     public OnTaskCompleted listener = null;
 
     @Override
@@ -27,22 +25,16 @@ class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
         if (myApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("https://chineseidioms-159403.appspot.com/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
                     });
-            // end options for devappserver
 
             myApiService = builder.build();
         }
-
-//        context = params[0];
 
         try {
             return myApiService.fetchIdiom().execute().getData();
@@ -51,13 +43,12 @@ class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
         }
     }
 
-    public EndpointsAsyncTask(OnTaskCompleted listener){
+    public EndpointsAsyncTask(OnTaskCompleted listener) {
         this.listener = listener;
     }
 
     @Override
     protected void onPostExecute(String result) {
-//        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
         listener.onTaskCompleted(result);
     }
 }
