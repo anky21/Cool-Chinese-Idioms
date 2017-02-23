8e87869ec5a7b2c64bd91bbe914b57a96ec3ea15
Anky <anky_au@hotmail.com>
2017-02-23 20:51:33 +1000

    feat: Show a Pause button when audio file is playing

--- app/src/main/java/me/anky/coolchineseidioms/HomeFragment.java
+++ app/src/main/java/me/anky/coolchineseidioms/HomeFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -27,14 +28,19 @@ public class HomeFragment extends Fragment implements OnTaskCompleted {
     // Handles audio focus when playing a sound file
     private AudioManager audioManager;
 
+    private int length = 0;
+
     @BindView(R.id.card_view)
     CardView cardView;
 
     @BindView(R.id.idiom_of_the_day)
     TextView idiomTv;
 
+    @BindView(R.id.sound_play_icon_frame)
+    FrameLayout soundPlayIconFrame;
+
     @BindView(R.id.sound_play_icon)
-    FrameLayout soundPlayIcon;
+    ImageView soundPlayIcon;
 
     private String idiom = "";
 
@@ -53,9 +59,13 @@ public class HomeFragment extends Fragment implements OnTaskCompleted {
                 // Pause playback and reset player to the start of the file
                 mediaPlayer.pause();
                 mediaPlayer.seekTo(0);
+                // Change audio icon
+                Utilities.setAudioPlayIcon(mediaPlayer, soundPlayIcon);
             } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
                 // Resume playback when focus is regained.
                 mediaPlayer.start();
+                // Change audio icon
+                Utilities.setAudioPlayIcon(mediaPlayer, soundPlayIcon);
             } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
                 // Stop playback and clean up resources
                 releaseMediaPlayer();
@@ -69,6 +79,10 @@ public class HomeFragment extends Fragment implements OnTaskCompleted {
     private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
+            // Reset position to the start
+            length = 0;
+            // Change audio icon
+            Utilities.setAudioPlayIcon(mediaPlayer, soundPlayIcon);
             // Release the media player resources.
             releaseMediaPlayer();
         }
@@ -86,24 +100,45 @@ public class HomeFragment extends Fragment implements OnTaskCompleted {
         new EndpointsAsyncTask(this).execute();
 
         // Change background colour of the CardView
-        cardView.setCardBackgroundColor(Color.CYAN);
+        cardView.setCardBackgroundColor(Color.WHITE);
 
+        soundPlayIcon.setTag(R.drawable.ic_play_sound);
 
-        soundPlayIcon.setOnClickListener(new View.OnClickListener() {
+        soundPlayIconFrame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Release the MediaPlayer if it currently exists
-                releaseMediaPlayer();
-                int result = audioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    // Create and setup the {@link MediaPlayer}
-                    mediaPlayer = MediaPlayer.create(getContext(), R.raw.duiniutanqin);
+                if ((int)soundPlayIcon.getTag() == R.drawable.ic_play_sound && length == 0) {
+                    // Release the MediaPlayer if it currently exists
+                    releaseMediaPlayer();
+                    int result = audioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                            AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                    if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                        // Create and setup the {@link MediaPlayer}
+                        mediaPlayer = MediaPlayer.create(getContext(), R.raw.duiniutanqin);
 
-                    // Start playing the audio file
+                        // Start playing the audio file
+                        mediaPlayer.start();
+
+                        // Change audio icon
+                        Utilities.setAudioPlayIcon(mediaPlayer, soundPlayIcon);
+
+                        // Setup a listener to stop and release the media player after playing
+                        mediaPlayer.setOnCompletionListener(mCompletionListener);
+                    }
+                } else if ((int)soundPlayIcon.getTag() == R.drawable.ic_play_sound && length != 0){
+                    // Start from the saved position
+                    mediaPlayer.seekTo(length);
                     mediaPlayer.start();
-                    // Setup a listener to stop and release the media player after playing
-                    mediaPlayer.setOnCompletionListener(mCompletionListener);
+
+                    // Change audio icon
+                    Utilities.setAudioPlayIcon(mediaPlayer, soundPlayIcon);
+                } else if(mediaPlayer.isPlaying()) {
+                    // Save the current position
+                    length = mediaPlayer.getCurrentPosition();
+                    mediaPlayer.pause();
+
+                    // Change audio icon
+                    Utilities.setAudioPlayIcon(mediaPlayer, soundPlayIcon);
                 }
             }
         });
@@ -127,6 +162,10 @@ public class HomeFragment extends Fragment implements OnTaskCompleted {
      * Clean up the media player by releasing its resources.
      */
     private void releaseMediaPlayer() {
+        // Change audio icon
+        soundPlayIcon.setImageResource(R.drawable.ic_play_sound);
+        soundPlayIcon.setTag(R.drawable.ic_play_sound);
+
         if (mediaPlayer != null) {
             // Release its resources
             mediaPlayer.release();
--- app/src/main/java/me/anky/coolchineseidioms/Utilities.java
+++ app/src/main/java/me/anky/coolchineseidioms/Utilities.java
@@ -1,5 +1,8 @@
 package me.anky.coolchineseidioms;
 
+import android.media.MediaPlayer;
+import android.widget.ImageView;
+
 /**
  * Created by Anky An on 22/02/2017.
  * anky25@gmail.com
@@ -7,4 +10,15 @@ package me.anky.coolchineseidioms;
 
 public class Utilities {
 
+    // Set audio play icon
+    public static void  setAudioPlayIcon(MediaPlayer mediaPlayer,ImageView imageView){
+        boolean isPlaying = mediaPlayer.isPlaying();
+        if(isPlaying){
+            imageView.setImageResource(R.drawable.ic_pause_sound);
+            imageView.setTag(R.drawable.ic_pause_sound);
+        } else {
+            imageView.setImageResource(R.drawable.ic_play_sound);
+            imageView.setTag(R.drawable.ic_play_sound);
+        }
+    }
 }
