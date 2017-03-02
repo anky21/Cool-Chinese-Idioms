1a80459575663d2605e1b404ad076d4f6c5ac687
Anky <anky_au@hotmail.com>
2017-03-03 06:56:26 +1000

    feat: Allow audio file in a list item to play
    Make minor changes in the IdiomProvider in the Java library

--- app/src/main/java/me/anky/coolchineseidioms/IdiomListCursorAdapter.java
+++ app/src/main/java/me/anky/coolchineseidioms/IdiomListCursorAdapter.java
@@ -2,13 +2,14 @@ package me.anky.coolchineseidioms;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import me.anky.coolchineseidioms.IdiomCollectionContract.IdiomCollectionEntry;
 
@@ -18,13 +19,17 @@ import me.anky.coolchineseidioms.IdiomCollectionContract.IdiomCollectionEntry;
  */
 
 public class IdiomListCursorAdapter extends CursorAdapter {
+    // Handles playback of all the sound files
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+
     MainViewHolder mainViewHolder;
 
-    public IdiomListCursorAdapter(Context context, Cursor c){
+    public IdiomListCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
 
-    public class MainViewHolder{
+    public class MainViewHolder {
         FrameLayout soundPlayButton;
         TextView idiomName;
     }
@@ -34,8 +39,8 @@ public class IdiomListCursorAdapter extends CursorAdapter {
         View rootView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
         mainViewHolder = new MainViewHolder();
 
-        mainViewHolder.soundPlayButton = (FrameLayout)rootView.findViewById(R.id.sound_play_icon_frame);
-        mainViewHolder.idiomName = (TextView)rootView.findViewById(R.id.idiom_name);
+        mainViewHolder.soundPlayButton = (FrameLayout) rootView.findViewById(R.id.sound_play_icon_frame);
+        mainViewHolder.idiomName = (TextView) rootView.findViewById(R.id.idiom_name);
 
         rootView.setTag(mainViewHolder);
 
@@ -44,6 +49,7 @@ public class IdiomListCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, final Context context, Cursor cursor) {
+        audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
 
         // Find the columns of idiom attributes
         int columnIdiom = cursor.getColumnIndex(IdiomCollectionEntry.COLUMN_IDIOM);
@@ -52,14 +58,78 @@ public class IdiomListCursorAdapter extends CursorAdapter {
         // Read the idiom attributes from the Cursor
         String idiomName = cursor.getString(columnIdiom);
         final String audioFile = cursor.getString(columnAudio);
+        final int audioId = context.getResources().getIdentifier(audioFile, "raw", context.getPackageName());
 
         // Update TextViews with the attributes for the current idiom
         mainViewHolder.idiomName.setText(idiomName);
         mainViewHolder.soundPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, audioFile, Toast.LENGTH_SHORT).show();
+
+                // Release the MediaPlayer if it currently exists
+                releaseMediaPlayer();
+                int result = audioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Create and setup the {@link MediaPlayer}
+                    mediaPlayer = MediaPlayer.create(context, audioId);
+
+                    // Start playing the audio file
+                    mediaPlayer.start();
+
+                    // Setup a listener to stop and release the media player after playing
+                    mediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
     }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+
+        if (mediaPlayer != null) {
+            // Release its resources
+            mediaPlayer.release();
+
+            // Set the media player back to null.
+            mediaPlayer = null;
+        }
+    }
+
+    /*
+    * This listener is triggered whenever the audio focus changes
+    * */
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // Pause playback and reset player to the start of the file
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // Resume playback when focus is regained.
+                mediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    /**
+     * Triggered when the {@link MediaPlayer} has completed playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Release the media player resources.
+            releaseMediaPlayer();
+
+            // Abandon the audio focus
+            audioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    };
 }
--- idiomprovider/src/main/java/me/anky/coolchineseidioms/IdiomProvider.java
+++ idiomprovider/src/main/java/me/anky/coolchineseidioms/IdiomProvider.java
@@ -1,22 +1,15 @@
 package me.anky.coolchineseidioms;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class IdiomProvider {
     public static String getIdiom() {
 
-        // Collection of the idioms
-        List<String> jokes = new ArrayList<>();
-        jokes.add("一鸣惊人");
-        jokes.add("好事成双");
-        jokes.add("一心一意");
-        jokes.add("坐井观天");
+        // Randomly create an integer according to the size of the idiom collection database
+        int min = 1;
+        int max = 20;
+        int rnd = new Random().nextInt(max - min + 1) + min;
 
-        // Randomly create an integer according to the size of the String array
-        int rnd = new Random().nextInt(jokes.size());
-
-        return jokes.get(rnd);
+        return String.valueOf(rnd);
     }
 }
