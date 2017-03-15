f56e720da2bd206c74a2a2a9bae6fba9859cc104
Anky <anky_au@hotmail.com>
2017-03-16 08:10:35 +1000

    feat: Create UI for Recording fragment

---
+++ app/src/main/java/me/anky/coolchineseidioms/RecordingFragment.java
@@ -0,0 +1,42 @@
+package me.anky.coolchineseidioms;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A fragment with a Google +1 button.
+ */
+public class RecordingFragment extends Fragment {
+
+    @BindView(R.id.button_listen)
+    AppCompatButton mButtonListen;
+
+    @BindView(R.id.button_record)
+    AppCompatButton mButtonRecord;
+
+    @BindView(R.id.button_play)
+    AppCompatButton mButtonPlay;
+
+    @BindView(R.id.button_close)
+    AppCompatButton mButtonClose;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_recording, container, false);
+        ButterKnife.bind(this, rootView);
+
+
+        return rootView;
+    }
+
+}
